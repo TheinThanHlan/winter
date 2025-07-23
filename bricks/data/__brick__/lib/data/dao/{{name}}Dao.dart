48eaa1bd11d11ed09f635a5edd565c42ba9bedf9
tth <theinthanhlan1@gmail.com>
2025-07-24 02:33:35 +0700

    add data brick

---
+++ bricks/data/__brick__/lib/data/dao/{{name}}Dao.dart
@@ -0,0 +1,4 @@
+import 'package:winter/winter.dart';
+class {{name}}Dao{
+  Database _db=getIt<Database>(instanceName:"");
+}
--- winter/lib/winter.dart
+++ winter/lib/winter.dart
@@ -1,10 +1,10 @@
 import "package:get_it/get_it.dart";
+export "package:sqflite/sqflite.dart";
 export 'package:get_it/get_it.dart';
 export 'context/Configurer.dart';
 export 'bean/Language.dart';
 export 'controller/WinterController.dart';
 export 'bean/WinterModel.dart';
 export 'db/WinterSqliteDatabaseProvider.dart';
-
 
 final GetIt getIt = GetIt.I;
