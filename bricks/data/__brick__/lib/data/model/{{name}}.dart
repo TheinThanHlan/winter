f74216f4c2e9ce6f0d786c6b24680f620a4d5dfa
tth <theinthanhlan1@gmail.com>
2025-07-24 20:39:39 +0700

    add json serialization to data brick

--- bricks/data/__brick__/lib/data/model/{{name}}.dart
+++ bricks/data/__brick__/lib/data/model/{{name}}.dart
@@ -1,4 +1,20 @@
 class {{name}}{
   int id;
   {{name}}(this.id);
+
+
+
+  factory {{name}}.fromJson(Map<String, dynamic> json) {
+    return OCA(
+      json["id"],
+    );
+  }
+
+
+
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+    };
+  }
 }
