c544d32e4d317041e1835a1d590f1d9f68b0ae13
tth <theinthanhlan1@gmail.com>
2026-01-08 02:57:53 +0700

    fix dao

--- bricks/data/__brick__/lib/data/dao/{{name}}Dao.dart
+++ bricks/data/__brick__/lib/data/dao/{{name}}Dao.dart
@@ -1,6 +1,6 @@
 import 'package:winter/winter.dart';
 class {{name}}Dao{
   Database _db;
-  {{name}}(this._db);
+  {{name}}Dao(this._db);
   //getIt<DatabaseProvider>(instanceName:"{{database_name}}").getDb();
 }
