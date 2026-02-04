ee8742d8d1bb6608aca22678c1659a8723280429
tth <theinthanhlan1@gmail.com>
2026-02-04 16:45:50 +0700

    update table

--- winter/lib/components/TableWithStickyHeader/TableWithStickyHeaderModel.dart
+++ winter/lib/components/TableWithStickyHeader/TableWithStickyHeaderModel.dart
@@ -40,16 +40,5 @@ class TableWithStickyHeaderModel implements WinterModel {
     this.tableBorder = const TableBorder(
       horizontalInside: BorderSide(color: Colors.black12),
     ),
-  }) : assert(() {
-         if (rows.isNotEmpty) {
-           if (rows.every((a) => columns.length != a.children.length)) {
-             throw FlutterError(
-               'Table contains irregular row lengths.\n'
-               'Every TableRow in a Table must have the same number of children, so that every cell is filled. '
-               'Otherwise, the table will contain holes.',
-             );
-           }
-         }
-         return true;
-       }());
+  });
 }
